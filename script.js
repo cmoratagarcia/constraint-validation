09f4166a9cc3fea269895b61f6c9f80a0b7617b4
Carmen Morata <cmoratagarcia@gmail.com>
2024-09-04 11:37:56 -0700

    Add event listeners (without function names_

--- script.js
+++ script.js
@@ -12,13 +12,28 @@ const APP = {
     APP.addListeners();
   },
   addListeners() {
-    let form = document.getElementsByClassName("form");
-    let email = document.getElementById("email");
-    let country = document.getElementById("country");
-    let zip = document.getElementById("zip");
-    let password = document.getElementById("password");
-    let confirmPassword = document.getElementById("confirm_password");
+    let form = document.getElementsByClassName('form')
+    let email = document.getElementById('email');
+    let country = document.getElementById('country');
+    let zip = document.getElementById('zip');
+    let password = document.getElementById('password');
+    let confirmPassword = document.getElementById('confirm_password');
+    
+    //after changing the whole value
+   email.addEventListener('change', APP.);
+   zip.addEventListener('change', APP.);
+   password.addEventListener('change', APP.);
+   confirmPassword.addEventListener('change', APP.);
+
+    //while typing?
+    
+
+    //what to do if something went wrong during validation
+  
+
+    //when the form gets submitted, in case of any last-minute changes. Add high five here
+    form.addEventListener('submit', APP.validate);
   },
 };
-document.addEventListener("DOMContentLoaded", APP.init);
+document.addEventListener('DOMContentLoaded', APP.init);
 //If all is well and the form is “submitted”, give the user a high five.
